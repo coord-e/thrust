8d2aa0a19c051d8e4271cf4c446a2ee124e07134
coord_e <me@coord-e.com>
2024-10-05 13:12:17 +0900

    fixup! Add some ADT cases

---
+++ tests/ui/fail/adt.rs
@@ -0,0 +1,36 @@
+//@error-in-other-file: Unsat
+//@compile-flags: -C debug-assertions=off
+
+#![feature(register_tool)]
+#![register_tool(thrust)]
+
+pub enum X {
+    A(i64),
+    B(bool),
+}
+
+#[thrust::trusted]
+#[thrust::requires(true)]
+#[thrust::ensures(true)]
+fn rand() -> X { unimplemented!() }
+
+fn inv(x: X) -> X {
+    match x {
+        X::A(i) => X::A(-i),
+        X::B(b) => X::B(!b),
+    }
+}
+
+fn is_pos(x: &X) -> bool {
+    match x {
+        X::A(i) => *i > 0,
+        X::B(b) => *b,
+    }
+}
+
+fn main() {
+    let x = rand();
+    if is_pos(&x) {
+        assert!(is_pos(&inv(x)));
+    }
+}
