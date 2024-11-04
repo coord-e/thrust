51079863693852ee3c6f075bbc3f04de7c7a4e1e
coord_e <me@coord-e.com>
2024-11-04 21:26:18 +0900

    do not emit sorts when empty

--- src/chc/format_context.rs
+++ src/chc/format_context.rs
@@ -93,6 +93,9 @@ struct Sorts<'a> {
 
 impl<'a> std::fmt::Display for Sorts<'a> {
     fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        if self.inner.is_empty() {
+            return Ok(());
+        }
         write!(f, "<")?;
         for (i, s) in self.inner.iter().enumerate() {
             if i != 0 {
