0210dfe4c62c8e046f7f99d09bf122a308491401
coord_e <me@coord-e.com>
2024-11-04 15:20:30 +0900

    list const cases

---
+++ tests/ui/fail/list_sum_const.rs
@@ -0,0 +1,28 @@
+//@error-in-other-file: Unsat
+//@compile-flags: -C debug-assertions=off -C opt-level=3
+//@rustc-env: REFA_SOLVER_TIMEOUT_SECS=60
+
+#![feature(register_tool)]
+#![register_tool(thrust)]
+
+enum List {
+  Cons(i32, Box<List>),
+  Nil,
+}
+
+#[thrust::trusted]
+#[thrust::requires(true)]
+#[thrust::ensures(true)]
+fn rand() -> i32 { unimplemented!() }
+
+fn sum(la: &List) -> i32 {
+  match la {
+    List::Cons(x, lb) => sum(lb) + *x,
+    List::Nil => 0,
+  }
+}
+
+fn main() {
+  let list = List::Cons(1, Box::new(List::Cons(2, Box::new(List::Nil))));
+  assert!(sum(&list) == 2);
+}
