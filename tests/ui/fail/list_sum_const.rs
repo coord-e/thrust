ae831a35a43b88ac2f6e37eacfe5a3415e1a3c1a
coord_e <me@coord-e.com>
2024-11-09 22:18:40 +0900

    no need to use eld in list_sum_const now

--- tests/ui/fail/list_sum_const.rs
+++ tests/ui/fail/list_sum_const.rs
@@ -1,7 +1,6 @@
 //@error-in-other-file: Unsat
 //@compile-flags: -C debug-assertions=off -C opt-level=3
 //@rustc-env: THRUST_SOLVER_TIMEOUT_SECS=120
-//@rustc-env: THRUST_SOLVER=eld
 
 enum List {
   Cons(i32, Box<List>),
