7270ca421b0de5d555b100edd17539739a589691
coord_e <me@coord-e.com>
2025-12-11 01:07:29 +0900

    Test parsing existentials

---
+++ tests/ui/fail/annot_exists.rs
@@ -0,0 +1,16 @@
+//@error-in-other-file: Unsat
+//@compile-flags: -C debug-assertions=off
+//@rustc-env: THRUST_SOLVER=thrust-pcsat-wrapper
+
+#[thrust::trusted]
+#[thrust::callable]
+fn rand() -> i32 { unimplemented!() }
+
+#[thrust::requires(true)]
+#[thrust::ensures(exists x:int. result == 2 * x)]
+fn f() -> i32 {
+    let x = rand();
+    x + x + x
+}
+
+fn main() {}
