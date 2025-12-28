79627ca13ac3cec64d7331b5c7784176b26fdf99
coord_e <me@coord-e.com>
2025-12-28 19:04:41 +0900

    Add more test cases

---
+++ tests/ui/pass/option_inc.rs
@@ -0,0 +1,17 @@
+//@check-pass
+//@compile-flags: -C debug-assertions=off
+
+fn maybe_inc(x: i32, do_it: bool) -> Option<i32> {
+    if do_it {
+        Some(x + 1)
+    } else {
+        None
+    }
+}
+
+fn main() {
+    let res = maybe_inc(10, true);
+    if let Some(v) = res {
+        assert!(v == 11);
+    }
+}
