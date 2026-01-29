4c67ff566f6453fda3063d3315db1a2a71ed861f
coeff-aij <175928954+coeff-aij@users.noreply.github.com>
2026-01-29 16:17:36 +0900

    add: tests for annotations in traits

---
+++ tests/ui/pass/annot_preds_trait.rs
@@ -0,0 +1,43 @@
+//@check-pass
+//@compile-flags: -Adead_code -C debug-assertions=off
+
+// A is represented as Tuple<Int> in SMT-LIB2 format.
+struct A {
+    x: i64,
+}
+
+trait Double {
+    // Support annotations in trait definitions
+    #[thrust::predicate]
+    fn is_double(self, doubled: Self) -> bool;
+
+    // This annotations are applied to all implementors of the `Double` trait.
+    #[thrust::requires(true)]
+    #[thrust::ensures(is_double(*self, ^self))]
+    fn double(&mut self);
+}
+
+impl Double for A {
+    // Write concrete definitions for predicates in `impl` blocks
+    #[thrust::predicate]
+    fn is_double(self, doubled: Self) -> bool {
+        // (tuple_proj<Int>.0 self) is equivalent to self.x
+        // self.x * 2 == doubled.x is written as following:
+        "(=
+            (* (tuple_proj<Int>.0 self) 2)
+            (tuple_proj<Int>.0 doubled)
+        )"; true
+    }
+
+    // Check if this method complies with annotations in
+    // trait definition.
+    fn double(&mut self) {
+        self.x += self.x;
+    }
+}
+
+fn main() {
+    let mut a = A { x: 3 };
+    a.double();
+    assert!(a.x == 6);
+}
