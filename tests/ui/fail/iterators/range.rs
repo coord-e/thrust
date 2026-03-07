b7cd96767ac996e74cb6b648ee7758bc73600932
coeff-aij <175928954+coeff-aij@users.noreply.github.com>
2026-03-07 16:55:58 +0900

    add: test cases for iterator verification(Range, FixedFilter)

---
+++ tests/ui/fail/iterators/range.rs
@@ -0,0 +1,35 @@
+//@error-in-other-file: Unsat
+//@compile-flags: -C debug-assertions=off
+
+struct Range {
+    start: i64,
+    end: i64,
+}
+
+impl Iterator for Range {
+    type Item = i64;
+
+    fn next(&mut self) -> Option<Self::Item> {
+        if self.start < self.end {
+            let item = self.start;
+            self.start += 1;
+            Some(item)
+        } else {
+            None
+        }
+    }
+}
+
+fn main() {
+    let mut range = Range {
+        start: 0,
+        end: 5,
+    };
+
+    let mut count = 0;
+    while let Some(i) = range.next() {
+        count += 1;
+    }
+
+    assert!(range.start != count);
+}
