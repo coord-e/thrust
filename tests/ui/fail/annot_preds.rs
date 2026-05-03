e7f0ba023a80189941b8868950c7b1f05e89784a
coord_e <me@coord-e.com>
2026-05-03 15:35:18 +0900

    Update annot_preds case to use thrust_macros

--- tests/ui/fail/annot_preds.rs
+++ tests/ui/fail/annot_preds.rs
@@ -1,4 +1,4 @@
-//@check-pass
+//@error-in-other-file: Unsat
 //@compile-flags: -Adead_code -C debug-assertions=off
 
 #[thrust::predicate]
@@ -12,10 +12,10 @@ fn is_double(x: i64, doubled_x: i64) -> bool {
 #[thrust_macros::requires(true)]
 #[thrust_macros::ensures(is_double(x, result))]
 fn double(x: i64) -> i64 {
-    x + x
+    x + x + x
 }
 
 fn main() {
     let a = 3;
-    assert!(double(a) == 6);
+    assert!(double(a) == 9);
 }
