debaa614289ab1185f025dc448599728620ac022
coeff-aij <175928954+coeff-aij@users.noreply.github.com>
2026-06-08 12:00:48 +0900

    change: use forall sort instead of deferred type

--- src/analyze/crate_.rs
+++ src/analyze/crate_.rs
@@ -120,16 +120,9 @@ impl<'tcx, 'ctx> Analyzer<'tcx, 'ctx> {
         };
 
         use mir_ty::TypeVisitableExt as _;
-        if sig.has_param() {
-            // TODO: needs clear criteria on whether extern_spec'ed target fn is analyzed or not
-            if target_def_id.as_local().is_none_or(|def_id| {
-                self.skip_analysis.contains(&def_id) || !self.tcx.is_mir_available(def_id)
-            }) {
-                self.ctx
-                    .register_deferred_def_without_analysis(target_def_id, local_def_id);
-            } else {
-                self.ctx.register_deferred_def(target_def_id, local_def_id);
-            }
+        if sig.has_param() && self.skip_analysis.contains(&local_def_id) {
+            self.ctx
+                .register_deferred_def_without_analysis(target_def_id, local_def_id);
         } else {
             let expected = analyzer.expected_ty();
             self.ctx.register_def(target_def_id, expected);
