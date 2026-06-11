1eb229bf7299d7dc056ddac423162b742cd82f4d
coeff-aij <175928954+coeff-aij@users.noreply.github.com>
2026-06-11 14:29:17 +0900

    add: more tests for traits

---
+++ tests/ui/pass/traits/simple_loop_2int.rs
@@ -0,0 +1,28 @@
+//@check-pass
+//@compile-flags: -C debug-assertions=off
+//@rustc-env: THRUST_SOLVER=tests/thrust-pcsat-wrapper THRUST_SOLVER_TIMEOUT_SECS=60
+
+#[thrust_macros::context]
+trait A {
+    #[thrust_macros::requires(Self::p(x, x))]
+    #[thrust_macros::ensures(Self::p(result, result))]
+    fn f(&self, x: i64) -> i64;
+
+    #[thrust_macros::predicate]
+    fn p(x: i64, y: i64) -> bool;
+}
+
+#[thrust_macros::requires(T::p(x, x))]
+#[thrust_macros::ensures(T::p(result, result))]
+fn target<T: A>(a: &T, x: i64) -> i64 {
+    let mut v = x;
+    let mut i = 0;
+    while i < 3 {
+        v = a.f(v);
+        i += 1;
+    }
+
+    v
+}
+
+fn main() {}
