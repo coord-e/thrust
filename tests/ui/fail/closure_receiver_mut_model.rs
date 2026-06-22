26c1cea9c6db160593d27ed6652b82baf392f64d
coord_e <me@coord-e.com>
2026-06-22 19:25:00 +0900

    Add test cases of #138

---
+++ tests/ui/fail/closure_receiver_mut_model.rs
@@ -0,0 +1,24 @@
+//@error-in-other-file: Unsat
+//@compile-flags: -C debug-assertions=off
+//@rustc-env: THRUST_SOLVER=tests/thrust-pcsat-wrapper
+
+use thrust_models::{exists, model::{Mut, Int}};
+
+#[thrust_macros::ensures(exists(|g, h, i: Int|
+  thrust_macros::post!(Mut::new(*f, g)(), i)
+  && thrust_macros::post!(Mut::new(h, !f)(), result)
+))]
+fn call_twice<F: FnMut() -> i32>(f: &mut F) -> i32 {
+    f();
+    f()
+}
+
+fn main() {
+    let mut x = 1;
+    let mut f = move || {
+        x += 1;
+        x
+    };
+    let r = call_twice(&mut f);
+    assert!(r == 3);
+}
