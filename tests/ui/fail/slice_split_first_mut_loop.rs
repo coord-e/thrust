c8f600abf435e59ec8df580f0be286dd11246cba
coord_e <me@coord-e.com>
2026-09-24 21:05:36 +0900

    Revert "Remove unstable slice_split_first_mut_loop.rs cases for now"
    This reverts commit 7d886e09d5647e3dfd22dcc9597b516bc95c7a5c.

---
+++ tests/ui/fail/slice_split_first_mut_loop.rs
@@ -0,0 +1,27 @@
+//@error-in-other-file: Unsat
+//@compile-flags: -C debug-assertions=off
+//@rustc-env: THRUST_SOLVER=tests/thrust-pcsat-wrapper
+
+#[thrust::trusted]
+#[thrust_macros::requires(true)]
+#[thrust_macros::ensures(
+    (*result).len() == 2
+        && (*result)[0] == 10
+        && (*result)[1] == 20
+)]
+fn slice() -> &'static mut [i32] {
+    unimplemented!()
+}
+
+#[thrust::callable]
+fn check() {
+    let sl = slice();
+    let mut s = &mut *sl;
+    while let Some((first, rest)) = s.split_first_mut() {
+        *first = 0;
+        s = rest;
+    }
+    assert!(sl[0] != 0);
+}
+
+fn main() {}
